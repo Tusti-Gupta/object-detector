628bf39496acf726396f5ca344f6eb37bf2e783c
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-12-09 15:57:29 +0800

    Add files via upload

---
+++ icecream.js
@@ -0,0 +1,25 @@
+img = "";
+status = "";
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function preload() {
+    img = loadImage("icecreamSample.jpeg");
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+}
+
+function back() {
+    window.location = "index.html";
+}
